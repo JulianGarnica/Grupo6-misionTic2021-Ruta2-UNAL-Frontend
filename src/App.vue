c9ffdaac7ade6d340a537d1f10dcf834c793bb70
Julian Garnica <jsgarnicacastillo@gmail.com>
2021-10-30 19:46:25 -0500

    login

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,139 @@
 <template>
-  <div id="nav">
+  <Menu v-if="!userData"></Menu>
+  <MenuLogueado :dataUser="userData" v-else></MenuLogueado>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  </div> -->
+  <div class="wrapper"  v-if="userData">
+    <Menu2></Menu2>
+    <div class="mainContent">
+      <router-view :dataUser="userData"/>
+    </div>
   </div>
-  <router-view/>
+
+  <div class="wrapperNoLog"  v-if="!userData">
+    <div class="mainContentNoLog">
+      <router-view dataUser=null />
+    </div>
+  </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import Menu from '@/components/menu.vue'
+import MenuLogueado from '@/components/menuLogueado.vue'
+import Menu2 from '@/components/menu2.vue'
+import VueJwtDecode from 'vue-jwt-decode'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      userData: null
+    }
+  },
+  components: {
+    Menu,
+    MenuLogueado,
+    Menu2
+  },
+  methods: {
+    getUserLogged () {
+      const accessToken = localStorage.access
+      const refreshToken = localStorage.refresh
+      if (accessToken != null && refreshToken != null) {
+        const UserData = VueJwtDecode.decode(accessToken)
+        //  const refreshData = VueJwtDecode.decode(refreshToken)
+        this.getUserData(UserData.user_id, accessToken, refreshToken)
+      }
+    },
+    getUserData (idUser, token, refreshToken) {
+      const authBearer = 'Bearer ' + token
+      axios.get('https://apptienditabackend.herokuapp.com/user/' + idUser,
+        {
+          headers: {
+            Authorization: authBearer,
+            'Content-Type': 'application/json'
+          }
+        }).then(result => {
+        this.userData = result.data
+        console.log(this.userData)
+        axios.post('https://apptienditabackend.herokuapp.com/refresh/', { refresh: refreshToken },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then(result => {
+          localStorage.access = result.data.access
+        }).catch(error => {
+          this.$swal({
+            icon: 'error',
+            title: 'Error',
+            text: 'Sesión cerrada por tiempo de inactividad, vuelve a iniciar sesión.'
+          })
+          localStorage.removeItem('refresh')
+          localStorage.removeItem('access')
+          console.log(error)
+        })
+      }).catch(error => {
+        this.$swal({
+          icon: 'error',
+          title: 'Error',
+          text: 'Sesión cerrada por tiempo de inactividad, vuelve a iniciar sesión.'
+        })
+        localStorage.removeItem('refresh')
+        localStorage.removeItem('access')
+        console.log(error)
+      })
+    }
+  },
+  mounted: function () {
+    this.$nextTick(this.getUserLogged())
+  }
+}
+</script>
+
 <style>
+*{
+  margin:0;
+  padding: 0;
+}
+
+body{
+  background-color: #F3FAFF;
+}
+
+.wrapper{
+  display: flex;
+  position: relative;
+}
+
+.mainContent{
+  width: 100%;
+  margin-left: 200px;
+  margin-top:80px;
+}
+
+.wrapperNoLog{
+  display: flex;
+  position: relative;
+}
+
+.mainContentNoLog{
+  width: 100%;
+  margin-top:80px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  border: 0;
+  padding: 0;
 }
 
 #nav {
@@ -24,7 +145,8 @@
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+a{
+  color: white;
+  text-decoration: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBox, faUsers, faPiggyBank, faUserTag, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(faBox, faUsers, faPiggyBank, faUserTag, faShoppingCart)
+createApp(App).use(store).use(router).use(VueSweetalert2).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
